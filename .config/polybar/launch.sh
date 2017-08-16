2e366dcceab5798523de8ceecf808bd114c6f487
Kim Nielsen <kgn@one.com>
2017-08-16 06:55:07 +0200

    Swiched to polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+for m in eDP1 DP1 DP2 HDMI3
+do
+    MONITOR=${m} polybar top &
+    MONITOR=${m} polybar bottom &
+done
+
+echo "Bars launched..."
