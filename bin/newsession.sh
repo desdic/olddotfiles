6acdb9647c2eee7336124685a2eca7eda6fc1335
Kim Nielsen <kgn@biq.dk>
2018-07-17 09:52:25 +0200

    Create new tmux session

---
+++ bin/newsession.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+[ ! -x /usr/bin/tmux ] && exit 1
+
+if /usr/bin/tmux ls|grep -q work:; then
+	/usr/bin/tmux new-window -t work
+else
+	LANG=da_DK.UTF-8 /usr/bin/st -- /usr/bin/tmux new-session -s work
+fi
+
+
