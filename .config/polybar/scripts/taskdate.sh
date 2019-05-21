d1765aa46be48047d6984a22246c8d795ead679b
Kim Nielsen <kgn@biq.dk>
2019-05-21 19:41:38 +0200

    Fix egde when task is overdue

--- .config/polybar/scripts/taskdate.sh
+++ .config/polybar/scripts/taskdate.sh
@@ -13,9 +13,9 @@ fi
 FORMAT=$(/usr/bin/date +"%V - %A %e %B %Y - %H:%M")
 
 if [ "$($TASK +READY +OVERDUE count)" -gt 0 ]; then
-  echo "%{F#F0DD74}%{B#627D82}%{B-}%{F-}${SYNC}%{F#cd1f3f}%{B#F0DD74}${OVERDUE} %{B-}%{F-}%{F#ABA167}%{B#F0DD74}%{F#000}%{B#ABA167}Week ${FORMAT} %{B-}%{F-}"
+  echo "%{F#F0DD74}%{B#2F343F}%{B-}%{F-}${SYNC}%{F#cd1f3f}%{B#F0DD74}${OVERDUE} %{B-}%{F-}%{F#ABA167}%{B#F0DD74}%{F#000}%{B#ABA167}Week ${FORMAT} %{B-}%{F-}"
 elif [ "$($TASK +DUETODAY count)" -gt 0 ]; then
-  echo "%{F#F0DD74}%{B#627D82}%{B-}%{F-}${SYNC}%{F#a3c725}%{B#F0DD74}${DUETODAY} %{B-}%{F-}%{F#ABA167}%{B#F0DD74}%{F#000}%{B#ABA167}Week ${FORMAT} %{B-}%{F-}"
+  echo "%{F#F0DD74}%{B#2F343F}%{B-}%{F-}${SYNC}%{F#a3c725}%{B#F0DD74}${DUETODAY} %{B-}%{F-}%{F#ABA167}%{B#F0DD74}%{F#000}%{B#ABA167}Week ${FORMAT} %{B-}%{F-}"
 else
   echo "%{F#F0DD74}%{B#2F343F}%{B-}%{F-}${SYNC}%{F#2F343F}%{B#F0DD74}${DUETODAY} %{B-}%{F-}%{F#ABA167}%{B#F0DD74}%{F#000}%{B#ABA167}Week ${FORMAT} %{B-}%{F-}"
 fi
