09fb972bd4466dca21d9ae51e2152d07830bf584
Kim Nielsen <kgn@one.com>
2019-07-22 18:59:09 +0200

    Back to urxvt again

--- bin/newsession.sh
+++ bin/newsession.sh
@@ -6,9 +6,9 @@ if /usr/bin/tmux ls|grep -q work:; then
   /usr/bin/tmux new-window -t work
 else
   #LANG=da_DK.UTF-8 /usr/bin/st -- /usr/bin/tmux new-session -s work
-  #LANG=da_DK.UTF-8 /usr/bin/urxvt -e /usr/bin/tmux new-session -s work
+  LANG=da_DK.UTF-8 /usr/bin/urxvt -name work -e /usr/bin/tmux new-session -s work
   #LANG=da_DK.UTF-8 /sbin/termite -e '/usr/bin/tmux new-session -s work'
-  LANG=da_DK.UTF-8 /sbin/kitty --class kittywork sh -c '/usr/bin/tmux new-session -s work'
+  #LANG=da_DK.UTF-8 /sbin/kitty --class kittywork sh -c '/usr/bin/tmux new-session -s work'
 fi
 
 
