45adf58c0d1913224ac034b093569be7ed9a51fe
Kim Nielsen <kgn@one.com>
2021-07-02 10:00:28 +0200

    Disable bouncekeys since I have a fixed keyboard

--- bin/keyboard.sh
+++ bin/keyboard.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-xset r rate 300 50
+#xset r rate 760 35
+#xset r rate 600 60
+#xset r rate 500 25
+#xset r rate 400 50
 xset s on
 xset s 600
-xkbcomp -w0 -I"$HOME/.xkb" -R"$HOME/.xkb" keymap/custom "$DISPLAY"
+#xkbset repeatkeys rate 600 25
+#xkbset bouncekeys 70
+#xkbset repeatkeys rate 400 25
+xkbset repeatkeys rate 600 25
+xkbcomp -w0 -I"$HOME/.xkb" -R"$HOME/.xkb" keymap/custom "$DISPLAY" >/dev/null 2>&1
