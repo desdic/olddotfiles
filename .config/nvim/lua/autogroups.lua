b8c0957130ccced15f8a27e8329e8bf98814b96c
Kim Nielsen <kgn@one.com>
2021-08-27 08:15:47 +0200

    Indentation

--- .config/nvim/lua/autogroups.lua
+++ .config/nvim/lua/autogroups.lua
@@ -15,122 +15,122 @@ if O.lua.autoformat then table.insert(auto_formatters, lua_format) end
 
 
 utils.define_augroups({
-  _general = {
-    {'BufEnter','*','set formatoptions-=cro'}, -- Don't continue comments
-    {'BufWritePre','/mnt/*','setlocal noundofile,setlocal shada="NONE"'}, -- Disable undo on these locations since
-    {'BufWritePre','/boot/*','setlocal noundofile,setlocal shada="NONE"'}, -- they are automounts
-    {'BufReadPost','*',':Watch %'} -- reload file on external changes
-  },
-  _text = {
-    {'FileType','text set tw=80 fo+=taw spell'},
-    {'FileType','text','set noexpandtab'},
-    {'FileType','text','set tabstop=4'},
-    {'FileType','text','set shiftwidth=4'}
-  },
-  _mail = {
-    {'FileType','mail set tw=72 fo+=taw spell'}
-  },
-  _gitcommit = {
-    {'FileType','gitcommit','set spell'}
-  },
-  _markdown = {
-    {'FileType','markdown set noexpandtab'},
-    {'FileType','markdown set tabstop=4'},
-    {'FileType','markdown set shiftwidth=4'}
-  },
-  _yaml = {
-    {'FileType','yaml','set expandtab'},
-    {'FileType','yaml','set tabstop=2'},
-    {'FileType','yaml','set shiftwidth=2'},
-    {'FileType','yaml','set softtabstop=2'}
-  },
-  _help = {
-    {'FileType','help','setlocal nolist'} --  don't show whitespace in help files
-  },
-  _json = {
-    {'FileType','json','set conceallevel=0'},
-    {'FileType','json','set ft=json'},
-    {'FileType','json','set expandtab'},
-    {'FileType','json','set tabstop=2'},
-    {'FileType','json','set shiftwidth=2'}
-  },
-  _sxhkdrc = {
-    {'FileType','sxhkdrc','set noexpandtab'},
-    {'FileType','sxhkdrc','set tabstop=2'},
-    {'FileType','sxhkdrc','set shiftwidth=2'}
-  },
-  _systemd = {
-    {'BufNewFile,BufRead','*/systemd/*.{automount,mount,path,service,socket,swap,target,timer} setfiletype systemd'},
-    {'BufNewFile,BufRead','/etc/systemd/system/*.d/*.conf setfiletype systemd'},
-    {'BufNewFile,BufRead','/etc/systemd/system/*.d/.#* setfiletype systemd'}
-  },
-  _toml = {
-    {'FileType','toml set expandtab'},
-    {'FileType','toml set shiftwidth=2'},
-    {'FileType','toml set softtabstop=2'},
-    {'FileType','toml set tabstop=2'}
-  },
-  _make = {
-    {'FileType','make set noexpandtab'},
-    {'FileType','make set tabstop=2'},
-    {'FileType','make set shiftwidth=2'}
-  },
-  _sh = {
-    {'FileType','sh','set expandtab'},
-    {'FileType','sh','set tabstop=2'},
-    {'FileType','sh','set shiftwidth=2'}
-  },
-  _python = {
-    {'FileType','python','set expandtab'},
-    {'FileType','python','set tabstop=4'},
-    {'FileType','python','set shiftwidth=4'}
-  },
-  _asm = {
-    {'FileType','asm','set expandtab'},
-    {'FileType','asm','set shiftwidth=4'},
-    {'FileType','asm','set softtabstop=4'},
-    {'FileType','asm','set tabstop=4'}
-  },
-  _c = {
-    {'FileType','c','setlocal expandtab'},
-    {'FileType','c','setlocal shiftwidth=4'},
-    {'FileType','c','setlocal softtabstop=4'},
-    {'FileType','c','setlocal tabstop=4'},
-    {'FileType','cpp','setlocal expandtab'},
-    {'FileType','cpp','setlocal shiftwidth=4'},
-    {'FileType','cpp','setlocal softtabstop=4'},
-    {'FileType','cpp','setlocal tabstop=4'}
-  },
-  _go = {
-    {'FileType','go','set nolist'},
-    {'FileType','go','set noexpandtab'},
-    {'FileType','go','set tabstop=4'},
-    {'FileType','go','set softtabstop=4'},
-    {'FileType','go','set shiftwidth=4'},
-    {'BufWritePre','*.go','FormatWrite'}
-  },
-  _lua = {
-    {'FileType','lua','set nolist'},
-    {'FileType','lua','set noexpandtab'},
-    {'FileType','lua','set tabstop=4'},
-    {'FileType','lua','set softtabstop=4'},
-    {'FileType','lua','set shiftwidth=4'}
-  },
-  _ruby = {
-    {'FileType','ruby','set expandtab'},
-    {'FileType','ruby','set shiftwidth=2'},
-    {'FileType','ruby','set softtabstop=2'},
-    {'FileType','ruby','set tabstop=2'},
-    {'FileType','eruby','set expandtab'},
-    {'FileType','eruby','set shiftwidth=2'},
-    {'FileType','eruby','set softtabstop=2'},
-    {'FileType','eruby','set tabstop=2'},
-    {'FileType','ruby.eruby.chef','set expandtab'},
-    {'FileType','ruby.eruby.chef','set shiftwidth=2'},
-    {'FileType','ruby.eruby.chef','set softtabstop=2'},
-    {'FileType','ruby.eruby.chef','set tabstop=2'}
-  },
-  _auto_formatters = auto_formatters
+	_general = {
+		{'BufEnter','*','set formatoptions-=cro'}, -- Don't continue comments
+		{'BufWritePre','/mnt/*','setlocal noundofile,setlocal shada="NONE"'}, -- Disable undo on these locations since
+		{'BufWritePre','/boot/*','setlocal noundofile,setlocal shada="NONE"'}, -- they are automounts
+		{'BufReadPost','*',':Watch %'} -- reload file on external changes
+	},
+	_text = {
+		{'FileType','text set tw=80 fo+=taw spell'},
+		{'FileType','text','set noexpandtab'},
+		{'FileType','text','set tabstop=4'},
+		{'FileType','text','set shiftwidth=4'}
+	},
+	_mail = {
+		{'FileType','mail set tw=72 fo+=taw spell'}
+	},
+	_gitcommit = {
+		{'FileType','gitcommit','set spell'}
+	},
+	_markdown = {
+		{'FileType','markdown set noexpandtab'},
+		{'FileType','markdown set tabstop=4'},
+		{'FileType','markdown set shiftwidth=4'}
+	},
+	_yaml = {
+		{'FileType','yaml','set expandtab'},
+		{'FileType','yaml','set tabstop=2'},
+		{'FileType','yaml','set shiftwidth=2'},
+		{'FileType','yaml','set softtabstop=2'}
+	},
+	_help = {
+		{'FileType','help','setlocal nolist'} --  don't show whitespace in help files
+	},
+	_json = {
+		{'FileType','json','set conceallevel=0'},
+		{'FileType','json','set ft=json'},
+		{'FileType','json','set expandtab'},
+		{'FileType','json','set tabstop=2'},
+		{'FileType','json','set shiftwidth=2'}
+	},
+	_sxhkdrc = {
+		{'FileType','sxhkdrc','set noexpandtab'},
+		{'FileType','sxhkdrc','set tabstop=2'},
+		{'FileType','sxhkdrc','set shiftwidth=2'}
+	},
+	_systemd = {
+		{'BufNewFile,BufRead','*/systemd/*.{automount,mount,path,service,socket,swap,target,timer} setfiletype systemd'},
+		{'BufNewFile,BufRead','/etc/systemd/system/*.d/*.conf setfiletype systemd'},
+		{'BufNewFile,BufRead','/etc/systemd/system/*.d/.#* setfiletype systemd'}
+	},
+	_toml = {
+		{'FileType','toml set expandtab'},
+		{'FileType','toml set shiftwidth=2'},
+		{'FileType','toml set softtabstop=2'},
+		{'FileType','toml set tabstop=2'}
+	},
+	_make = {
+		{'FileType','make set noexpandtab'},
+		{'FileType','make set tabstop=2'},
+		{'FileType','make set shiftwidth=2'}
+	},
+	_sh = {
+		{'FileType','sh','set expandtab'},
+		{'FileType','sh','set tabstop=2'},
+		{'FileType','sh','set shiftwidth=2'}
+	},
+	_python = {
+		{'FileType','python','set expandtab'},
+		{'FileType','python','set tabstop=4'},
+		{'FileType','python','set shiftwidth=4'}
+	},
+	_asm = {
+		{'FileType','asm','set expandtab'},
+		{'FileType','asm','set shiftwidth=4'},
+		{'FileType','asm','set softtabstop=4'},
+		{'FileType','asm','set tabstop=4'}
+	},
+	_c = {
+		{'FileType','c','setlocal expandtab'},
+		{'FileType','c','setlocal shiftwidth=4'},
+		{'FileType','c','setlocal softtabstop=4'},
+		{'FileType','c','setlocal tabstop=4'},
+		{'FileType','cpp','setlocal expandtab'},
+		{'FileType','cpp','setlocal shiftwidth=4'},
+		{'FileType','cpp','setlocal softtabstop=4'},
+		{'FileType','cpp','setlocal tabstop=4'}
+	},
+	_go = {
+		{'FileType','go','set nolist'},
+		{'FileType','go','set noexpandtab'},
+		{'FileType','go','set tabstop=4'},
+		{'FileType','go','set softtabstop=4'},
+		{'FileType','go','set shiftwidth=4'},
+		{'BufWritePre','*.go','FormatWrite'}
+	},
+	_lua = {
+		{'FileType','lua','set nolist'},
+		{'FileType','lua','set noexpandtab'},
+		{'FileType','lua','set tabstop=4'},
+		{'FileType','lua','set softtabstop=4'},
+		{'FileType','lua','set shiftwidth=4'}
+	},
+	_ruby = {
+		{'FileType','ruby','set expandtab'},
+		{'FileType','ruby','set shiftwidth=2'},
+		{'FileType','ruby','set softtabstop=2'},
+		{'FileType','ruby','set tabstop=2'},
+		{'FileType','eruby','set expandtab'},
+		{'FileType','eruby','set shiftwidth=2'},
+		{'FileType','eruby','set softtabstop=2'},
+		{'FileType','eruby','set tabstop=2'},
+		{'FileType','ruby.eruby.chef','set expandtab'},
+		{'FileType','ruby.eruby.chef','set shiftwidth=2'},
+		{'FileType','ruby.eruby.chef','set softtabstop=2'},
+		{'FileType','ruby.eruby.chef','set tabstop=2'}
+	},
+	_auto_formatters = auto_formatters
 })
 
 -- Highlight on yank
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,263 +1,262 @@
-local packer_path =
-  vim.fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
+local packer_path = vim.fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
 if vim.fn.empty(vim.fn.glob(packer_path)) then
-  vim.fn.system {
-    'git',
-    'clone',
-    'https://github.com/wbthomason/packer.nvim',
-    packer_path
-  }
-  vim.cmd('packadd packer.nvim')
+	vim.fn.system {
+		'git',
+		'clone',
+		'https://github.com/wbthomason/packer.nvim',
+		packer_path
+	}
+	vim.cmd('packadd packer.nvim')
 end
 
 return require('packer').startup {
-  function(use)
-    use {'wbthomason/packer.nvim', opt = true}
+	function(use)
+		use {'wbthomason/packer.nvim', opt = true}
 
-    use {'p00f/nvim-ts-rainbow'}
+		use {'p00f/nvim-ts-rainbow'}
 
-    use {
-      'nvim-treesitter/nvim-treesitter',
-      requires = {
-        'nvim-treesitter/nvim-treesitter-refactor',
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        'windwp/nvim-ts-autotag'
-      },
-      run = ':TSUpdate',
-      config = function()
-        require('config.treesitter')
-      end
-    }
+		use {
+			'nvim-treesitter/nvim-treesitter',
+			requires = {
+				'nvim-treesitter/nvim-treesitter-refactor',
+				'nvim-treesitter/nvim-treesitter-textobjects',
+				'windwp/nvim-ts-autotag'
+			},
+			run = ':TSUpdate',
+			config = function()
+				require('config.treesitter')
+			end
+		}
 
-    use {'ggandor/lightspeed.nvim'}
-    use {'SirVer/ultisnips'}
-    use {'honza/vim-snippets'}
+		use {'ggandor/lightspeed.nvim'}
+		use {'SirVer/ultisnips'}
+		use {'honza/vim-snippets'}
 
-    use {'lukas-reineke/indent-blankline.nvim',
-      config = function()
-        require('indent_blankline').setup {
-          char = '|',
-          buftype_exclude = {'terminal'}
-        }
-      end
-    }
+		use {'lukas-reineke/indent-blankline.nvim',
+			config = function()
+				require('indent_blankline').setup {
+					char = '|',
+					buftype_exclude = {'terminal'}
+				}
+			end
+		}
 
-    use {'golang/vscode-go'}
-    use {'rafamadriz/friendly-snippets'}
+		use {'golang/vscode-go'}
+		use {'rafamadriz/friendly-snippets'}
 
-    use {'editorconfig/editorconfig-vim'}
-    use 'Vimjas/vim-python-pep8-indent'
+		use {'editorconfig/editorconfig-vim'}
+		use 'Vimjas/vim-python-pep8-indent'
 
-    use {'b3nj5m1n/kommentary',
-      config = function()
-        require('kommentary.config').configure_language('sxhkdrc', {
-          prefer_single_line_comments = true,
-          use_consistent_indentation = true,
-        })
+		use {'b3nj5m1n/kommentary',
+			config = function()
+				require('kommentary.config').configure_language('sxhkdrc', {
+					prefer_single_line_comments = true,
+					use_consistent_indentation = true,
+				})
 
-        require('kommentary.config').configure_language('default', {
-          prefer_single_line_comments = true,
-          use_consistent_indentation = true,
-        })
-      end
-    }
+				require('kommentary.config').configure_language('default', {
+					prefer_single_line_comments = true,
+					use_consistent_indentation = true,
+				})
+			end
+		}
 
-    use {'christianchiarulli/nvcode-color-schemes.vim'}
-    use {'glepnir/galaxyline.nvim',
-      config = function()
-        require('config.galaxyline')
-      end
-    }
-    use {'tomasiser/vim-code-dark'}
+		use {'christianchiarulli/nvcode-color-schemes.vim'}
+		use {'glepnir/galaxyline.nvim',
+			config = function()
+				require('config.galaxyline')
+			end
+		}
+		use {'tomasiser/vim-code-dark'}
 
-    use {
-      'akinsho/nvim-bufferline.lua',
-      requires = 'kyazdani42/nvim-web-devicons',
-      config = function()
-        require('bufferline').setup {}
-      end
-    }
+		use {
+			'akinsho/nvim-bufferline.lua',
+			requires = 'kyazdani42/nvim-web-devicons',
+			config = function()
+				require('bufferline').setup {}
+			end
+		}
 
-    use {'neovim/nvim-lspconfig',
-      config = function()
-        require('config.nvim-lspconfig')
-      end
-    }
+		use {'neovim/nvim-lspconfig',
+			config = function()
+				require('config.nvim-lspconfig')
+			end
+		}
 
-    use {'kabouzeid/nvim-lspinstall'}
-    use {'glepnir/lspsaga.nvim'}
-    use {'mhartington/formatter.nvim'}
-    use {
-      'onsails/lspkind-nvim',
-      config = function()
-        local i = require('config.icons')
+		use {'kabouzeid/nvim-lspinstall'}
+		use {'glepnir/lspsaga.nvim'}
+		use {'mhartington/formatter.nvim'}
+		use {
+			'onsails/lspkind-nvim',
+			config = function()
+				local i = require('config.icons')
 
-        require('lspkind').init {
-          symbol_map = {
-            Class = i.lang.class,
-            Color = i.lang.color,
-            Constant = i.lang.constant,
-            Constructor = i.lang.constructor,
-            Enum = i.lang.enum,
-            EnumMember = i.lang.enummember,
-            Event = i.lang.event,
-            Field = i.lang.field,
-            File = i.lang.file,
-            Folder = i.lang.folder,
-            Function = i.lang['function'],
-            Interface = i.lang.interface,
-            Keyword = i.lang.keyword,
-            Method = i.lang.method,
-            Module = i.lang.module,
-            Operator = i.lang.operator,
-            Property = i.lang.property,
-            Reference = i.lang.reference,
-            Snippet = i.lang.snippet,
-            Struct = i.lang.struct,
-            Text = i.lang.text,
-            TypeParameter = i.lang.typeparameter,
-            Unit = i.lang.unit,
-            Value = i.lang.value,
-            Variable = i.lang.variable
-          }
-        }
-      end
-    }
+				require('lspkind').init {
+					symbol_map = {
+						Class = i.lang.class,
+						Color = i.lang.color,
+						Constant = i.lang.constant,
+						Constructor = i.lang.constructor,
+						Enum = i.lang.enum,
+						EnumMember = i.lang.enummember,
+						Event = i.lang.event,
+						Field = i.lang.field,
+						File = i.lang.file,
+						Folder = i.lang.folder,
+						Function = i.lang['function'],
+						Interface = i.lang.interface,
+						Keyword = i.lang.keyword,
+						Method = i.lang.method,
+						Module = i.lang.module,
+						Operator = i.lang.operator,
+						Property = i.lang.property,
+						Reference = i.lang.reference,
+						Snippet = i.lang.snippet,
+						Struct = i.lang.struct,
+						Text = i.lang.text,
+						TypeParameter = i.lang.typeparameter,
+						Unit = i.lang.unit,
+						Value = i.lang.value,
+						Variable = i.lang.variable
+					}
+				}
+			end
+		}
 
-    use {
-      'hrsh7th/nvim-cmp',
-      requires = {
-        'hrsh7th/vim-vsnip',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-nvim-lua',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-calc',
-        'hrsh7th/cmp-vsnip'
-      },
-      after = 'lspkind-nvim',
-      config = function()
-        require('cmp_nvim_lsp').setup()
-        local cmp = require('cmp')
-        local lspkind = require('lspkind')
-        local compare = require('cmp.config.compare')
+		use {
+			'hrsh7th/nvim-cmp',
+			requires = {
+				'hrsh7th/vim-vsnip',
+				'hrsh7th/cmp-nvim-lsp',
+				'hrsh7th/cmp-buffer',
+				'hrsh7th/cmp-nvim-lua',
+				'hrsh7th/cmp-path',
+				'hrsh7th/cmp-calc',
+				'hrsh7th/cmp-vsnip'
+			},
+			after = 'lspkind-nvim',
+			config = function()
+				require('cmp_nvim_lsp').setup()
+				local cmp = require('cmp')
+				local lspkind = require('lspkind')
+				local compare = require('cmp.config.compare')
 
-        cmp.setup {
-          preselect = false,
-          snippet = {
-            expand = function(args)
-              -- You must install `vim-vsnip` if you use the following as-is.
-              vim.fn['vsnip#anonymous'](args.body)
-            end
-          },
-          formatting = {
-            format = function(_, vim_item)
-              vim_item.kind = lspkind.presets.default[vim_item.kind]
-              return vim_item
-            end
-          },
-          sorting = {
-            priority_weight = 2.,
-            comparators = {
-              compare.offset,
-              compare.exact,
-              compare.score,
-              compare.kind,
-              compare.sort_text,
-              compare.length,
-              compare.order,
-            },
-          },
-          min_length = 0, -- allow for `from package import _` in Python
-          mapping = {
-            ['<S-TAB>'] = cmp.mapping.select_prev_item(),
-            ['<TAB>'] = cmp.mapping.select_next_item(),
-            ['<C-k>'] = cmp.mapping.confirm({
-              behavior = cmp.ConfirmBehavior.Insert,
-              select = true,
-            }),
-            ['<CR>'] = cmp.mapping.confirm {
-              behavior = cmp.ConfirmBehavior.Replace,
-              select = true
-            }
-          },
-          sources = {
-            {name = 'buffer'},
-            {name = 'nvim_lsp'},
-            {name = 'nvim_lua'},
-            {name = 'vsnip'},
-            {name = 'path'},
-            {name = 'calc'},
-          }
-        }
-      end
-    }
+				cmp.setup {
+					preselect = false,
+					snippet = {
+						expand = function(args)
+							-- You must install `vim-vsnip` if you use the following as-is.
+							vim.fn['vsnip#anonymous'](args.body)
+						end
+					},
+					formatting = {
+						format = function(_, vim_item)
+							vim_item.kind = lspkind.presets.default[vim_item.kind]
+							return vim_item
+						end
+					},
+					sorting = {
+						priority_weight = 2.,
+						comparators = {
+							compare.offset,
+							compare.exact,
+							compare.score,
+							compare.kind,
+							compare.sort_text,
+							compare.length,
+							compare.order,
+						},
+					},
+					min_length = 0, -- allow for `from package import _` in Python
+					mapping = {
+						['<S-TAB>'] = cmp.mapping.select_prev_item(),
+						['<TAB>'] = cmp.mapping.select_next_item(),
+						['<C-k>'] = cmp.mapping.confirm({
+							behavior = cmp.ConfirmBehavior.Insert,
+							select = true,
+						}),
+						['<CR>'] = cmp.mapping.confirm {
+							behavior = cmp.ConfirmBehavior.Replace,
+							select = true
+						}
+					},
+					sources = {
+						{name = 'buffer'},
+						{name = 'nvim_lsp'},
+						{name = 'nvim_lua'},
+						{name = 'vsnip'},
+						{name = 'path'},
+						{name = 'calc'},
+					}
+				}
+			end
+		}
 
-    use {
-      'folke/trouble.nvim',
-      requires = 'kyazdani42/nvim-web-devicons',
-      config = function()
-        local i = require('config.icons')
+		use {
+			'folke/trouble.nvim',
+			requires = 'kyazdani42/nvim-web-devicons',
+			config = function()
+				local i = require('config.icons')
 
-        require('trouble').setup {
-          signs = {
-            error = i.diag.error,
-            warning = i.diag.warn,
-            hint = i.diag.hint,
-            information = i.diag.info,
-            other = i.diag.pass
-          }
-        }
-      end
-    }
+				require('trouble').setup {
+					signs = {
+						error = i.diag.error,
+						warning = i.diag.warn,
+						hint = i.diag.hint,
+						information = i.diag.info,
+						other = i.diag.pass
+					}
+				}
+			end
+		}
 
-    use {
-      'nvim-telescope/telescope.nvim',
-      requires = {
-        'nvim-lua/plenary.nvim',
-        'nvim-telescope/telescope-fzy-native.nvim',
-        'nvim-telescope/telescope-media-files.nvim',
-        'nvim-telescope/telescope-github.nvim'
-        -- 'nvim-telescope/telescope-packer.nvim'  -- currently breaking packer
-      },
-      config = function()
-        require('config.telescope')
-      end
-    }
+		use {
+			'nvim-telescope/telescope.nvim',
+			requires = {
+				'nvim-lua/plenary.nvim',
+				'nvim-telescope/telescope-fzy-native.nvim',
+				'nvim-telescope/telescope-media-files.nvim',
+				'nvim-telescope/telescope-github.nvim'
+				-- 'nvim-telescope/telescope-packer.nvim'  -- currently breaking packer
+			},
+			config = function()
+				require('config.telescope')
+			end
+		}
 
-    use { 'ray-x/lsp_signature.nvim' }
-    use { 'rcarriga/nvim-notify' }
-    use {
-      'kshenoy/vim-signature',
-      config = function()
-        -- only supports vim-gitgutter
-        -- vim.g.SignatureMarkTextHLDynamic = 1
-      end
-    }
+		use { 'ray-x/lsp_signature.nvim' }
+		use { 'rcarriga/nvim-notify' }
+		use {
+			'kshenoy/vim-signature',
+			config = function()
+				-- only supports vim-gitgutter
+				-- vim.g.SignatureMarkTextHLDynamic = 1
+			end
+		}
 
-    use {
-      'lewis6991/gitsigns.nvim',
-      requires = 'nvim-lua/plenary.nvim',
-      config = function()
-        require('config.gitsigns')
-      end
-    }
+		use {
+			'lewis6991/gitsigns.nvim',
+			requires = 'nvim-lua/plenary.nvim',
+			config = function()
+				require('config.gitsigns')
+			end
+		}
 
-    use {
-      'windwp/nvim-autopairs',
-      after = 'nvim-cmp',
-      config = function()
-        require('nvim-autopairs').setup {}
+		use {
+			'windwp/nvim-autopairs',
+			after = 'nvim-cmp',
+			config = function()
+				require('nvim-autopairs').setup {}
 
-        -- handle <CR> mapping with nvim-cmp
-        require('nvim-autopairs.completion.cmp').setup {
-          map_cr = true, --  map <CR> on insert mode
-          map_complete = true -- insert `(` when function/method is completed
-        }
-      end
-    }
-  end,
-  config = {
-    python_cmd = 'python3'
-  }
+				-- handle <CR> mapping with nvim-cmp
+				require('nvim-autopairs.completion.cmp').setup {
+					map_cr = true, --  map <CR> on insert mode
+					map_complete = true -- insert `(` when function/method is completed
+				}
+			end
+		}
+	end,
+	config = {
+		python_cmd = 'python3'
+	}
 }
