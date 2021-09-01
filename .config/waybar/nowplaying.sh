3be3ee3b965f0196764420f252a75e06432f9868
Kim Nielsen <kgn@one.com>
2021-09-01 09:09:24 +0200

    New clean bar

--- .config/waybar/nowplaying.sh
+++ .config/waybar/nowplaying.sh
@@ -2,13 +2,13 @@
 
 set -euo pipefail
 
-state=$(playerctl metadata --format '{{playerName}}: {{title}} by {{artist}}')
+state=$(playerctl metadata --format ' {{ emoji(status) }} {{markup_escape(title)}} - {{markup_escape(artist)}}'| sed -e 's/▶️//g' -e 's/⏸️//g' )
 player_status=$(playerctl -a status)
 
 if [[ "$player_status" =~ "Playing" ]]; then
-    printf " %s" "$state"  2>/dev/null
+    printf "%s" "$state"  2>/dev/null
 elif [[ "$player_status" =~ "Paused" ]]; then
-    printf " %s" "$state"  2>/dev/null
+    printf "%s" "$state"  2>/dev/null
 else
     printf "$player_status"
 fi
