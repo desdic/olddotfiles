d17a8573b0c5ca9ea8236870a54dc90815fdf8f5
Kim Nielsen <kgn@one.com>
2021-11-22 13:40:14 +0100

    Use build-in

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -52,7 +52,6 @@ return require('packer').startup {
 		use {'lukas-reineke/indent-blankline.nvim',
 			config = function()
 				require('indent_blankline').setup {
-					char = '|',
 					buftype_exclude = {'terminal', 'popup'}
 				}
 			end
