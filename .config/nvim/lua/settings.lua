d49e799d009bee6ac598430e7839d0f265232469
Kim Nielsen <kgn@one.com>
2021-12-02 15:07:01 +0100

    Turn debug off

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -7,7 +7,7 @@ local buffer_option = vim.bo
 local window_option = vim.wo
 local indent = 2
 
-vim.lsp.set_log_level("debug")
+-- vim.lsp.set_log_level("debug")
 
 global.mapleader = ',' -- Set leader
 global.shada = "NONE"
