1fa0c230f4098fd918abf4fcd542722dcc2ebd3d
Kim Nielsen <kgn@one.com>
2021-12-07 10:56:02 +0100

    Update with new diagnostic names

--- .config/nvim/lua/config/nvim-lspconfig.lua
+++ .config/nvim/lua/config/nvim-lspconfig.lua
@@ -5,41 +5,38 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 	}
 )
 
--- local function documentHighlight(client, bufnr)
--- 	-- Set autocommands conditional on server_capabilities
--- 	if client.resolved_capabilities.document_highlight then
--- 		vim.api.nvim_exec(
--- 			[[
--- 			hi LspReferenceRead cterm=bold ctermbg=red guibg=#464646
--- 			hi LspReferenceText cterm=bold ctermbg=red guibg=#464646
--- 			hi LspReferenceWrite cterm=bold ctermbg=red guibg=#464646
--- 			augroup lsp_document_highlight
--- 			autocmd! * <buffer>
--- 			autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
--- 			autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
--- 			augroup END
--- 			]],
--- 			false
--- 		)
--- 	end
--- end
+vim.lsp.handlers["textDocument/hover"] =
+  vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {
+    border = "single",
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] =
+  vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  {
+    border = "single"
+  }
+)
 
 local i = require('config.icons')
 vim.fn.sign_define(
-	"LspDiagnosticsSignError",
-	{texthl = "LspDiagnosticsSignError", text = i.diag.error, numhl = "LspDiagnosticsSignError"}
+	"DiagnosticSignError",
+	{texthl = "DiagnosticSignError", text = i.diag.error, numhl = "DiagnosticSignError"}
 )
 vim.fn.sign_define(
-	"LspDiagnosticsSignWarning",
-	{texthl = "LspDiagnosticsSignWarning", text = i.diag.warn, numhl = "LspDiagnosticsSignWarning"}
+	"DiagnosticSignWarn",
+	{texthl = "DiagnosticSignWarn", text = i.diag.warn, numhl = "DiagnosticSignWarn"}
 )
 vim.fn.sign_define(
-	"LspDiagnosticsSignHint",
-	{texthl = "LspDiagnosticsSignHint", text = i.diag.hint, numhl = "LspDiagnosticsSignHint"}
+	"DiagnosticSignHint",
+	{texthl = "DiagnosticSignHint", text = i.diag.hint, numhl = "DiagnosticSignHint"}
 )
 vim.fn.sign_define(
-	"LspDiagnosticsSignInformation",
-	{texthl = "LspDiagnosticsSignInformation", text = i.diag.info, numhl = "LspDiagnosticsSignInformation"}
+	"DiagnosticSignInfo",
+	{texthl = "DiagnosticSignInfo", text = i.diag.info, numhl = "DiagnosticSignInfo"}
 )
 
 -- Set Default Prefix.
