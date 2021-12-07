3f0567020ac7c1d75b0050baad027da2c4147aa0
Kim Nielsen <kgn@one.com>
2021-12-07 10:51:57 +0100

    Add debounce

--- .config/nvim/lua/lsp/python.lua
+++ .config/nvim/lua/lsp/python.lua
@@ -3,6 +3,9 @@ local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protoco
 require'lspconfig'.pyright.setup {
 	cmd = {"node", DATA_PATH .. "/lsp_servers/python/node_modules/pyright/langserver.index.js", "--stdio"},
 	capabilities = capabilities,
+    flags = {
+      debounce_text_changes = O.python.debounce_text_changes,
+    },
 	handlers = {
 		["textDocument/publishDiagnostics"] = vim.lsp.with(
 			vim.lsp.diagnostic.on_publish_diagnostics, {
