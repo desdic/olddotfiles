9ebae1b4bca7cc5680a368ddb83e61014ce05dc1
Kim Nielsen <kgn@one.com>
2021-12-21 13:58:10 +0100

    Indentation

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -77,49 +77,49 @@ return require('packer').startup {
 
 				-- configure it
 				catppuccin.setup({
-						transparent_background = false,
-						term_colors = false,
-						styles = {
-							comments = "italic",
-							functions = "italic",
-							keywords = "italic",
-							strings = "NONE",
-							variables = "NONE",
+					transparent_background = false,
+					term_colors = false,
+					styles = {
+						comments = "italic",
+						functions = "italic",
+						keywords = "italic",
+						strings = "NONE",
+						variables = "NONE",
+					},
+					integrations = {
+						treesitter = true,
+						native_lsp = {
+							enabled = true,
+							virtual_text = {
+								errors = "italic",
+								hints = "italic",
+								warnings = "italic",
+								information = "italic",
+							},
+							underlines = {
+								errors = "underline",
+								hints = "underline",
+								warnings = "underline",
+								information = "underline",
+							},
 						},
-						integrations = {
-							treesitter = true,
-							native_lsp = {
-								enabled = true,
-								virtual_text = {
-									errors = "italic",
-									hints = "italic",
-									warnings = "italic",
-									information = "italic",
-								},
-								underlines = {
-									errors = "underline",
-									hints = "underline",
-									warnings = "underline",
-									information = "underline",
-								},
-							},
-							lsp_saga = false,
-							gitsigns = true,
-							telescope = true,
-							nvimtree = {
-								enabled = true,
-								show_root = false,
-							},
-							indent_blankline = {
-								enabled = false,
-								colored_indent_levels = false,
-							},
-							bufferline = false,
-							markdown = false,
-							lightspeed = true,
-							ts_rainbow = true,
+						lsp_saga = false,
+						gitsigns = true,
+						telescope = true,
+						nvimtree = {
+							enabled = true,
+							show_root = false,
 						},
-					})
+						indent_blankline = {
+							enabled = false,
+							colored_indent_levels = false,
+						},
+						bufferline = false,
+						markdown = false,
+						lightspeed = true,
+						ts_rainbow = true,
+					},
+				})
 			end
 		}
 
