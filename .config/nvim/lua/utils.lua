c3023dcf6bcfde43926c65ff4017edfb1b036ed4
Kim Nielsen <kgn@one.com>
2021-12-21 13:58:57 +0100

    Now using null-ls for goimports

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,21 +1,3 @@
-function goimports(wait_ms)
-	local params = vim.lsp.util.make_range_params()
-	params.context = {only = {"source.organizeImports"}}
-	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-	for _, res in pairs(result or {}) do
-		for _, r in pairs(res.result or {}) do
-			if r.edit then
-				vim.lsp.util.apply_workspace_edit(r.edit)
-			else
-				vim.lsp.buf.execute_command(r.command)
-			end
-		end
-	end
-	-- Always do formating
-	-- vim.lsp.buf.formatting()
-end
-
-
 -- Watch a file for changes
 local w = vim.loop.new_fs_event()
 local function on_change(err, fname, status)
