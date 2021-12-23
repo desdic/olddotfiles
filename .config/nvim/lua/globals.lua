4c5f6b8ed03a78fc3e7aa96168fc1dffb06cd88d
Kim Nielsen <kgn@one.com>
2021-12-23 14:38:18 +0100

    Cleanup globals

--- .config/nvim/lua/globals.lua
+++ .config/nvim/lua/globals.lua
@@ -1,13 +1,3 @@
 DATA_PATH = vim.fn.stdpath("data")
 CACHE_PATH = vim.fn.stdpath("cache")
 HOME_PATH = "/home/" .. vim.fn.expand("$USER")
-
-O = {
-    auto_close_tree = 0,
-    auto_complete = true,
-    hidden_files = true,
-    wrap_lines = false,
-    shell = "zsh",
-    timeoutlen = 100,
-    nvim_tree_disable_netrw = 0 -- "1 by default, disables netrw (must be set before plugin's packadd)
-}
